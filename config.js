72ce208d908a49a9fda7ab8747348d10a7dce24d
WA5CAF <cpmafrank@gmail.com>
2024-06-12 23:42:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,6 @@ const aURL = [
   ],
   ["2196F3", "DX CLUSTER", "https://dxcluster.ha8tks.hu/map/", "1"],
   
-  ["2196F3", "MOBILE SHACK", "https://cdn-bio.qrz.com/f/wa5caf/Mobile_Shack.jpg?p=972b3818494dff0ffc521d0b3eb845eb", "1"],
   [
     "2196F3",
     "LIGHTNING",
