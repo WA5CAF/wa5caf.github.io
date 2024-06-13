b5e2184712371f77e137225f09ec88fd2a85e23b
WA5CAF <cpmafrank@gmail.com>
2024-06-12 22:33:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const aURL = [
   ["add10d", "BACK", "#", "1", "R"],
   ["ff9100", "Refresh", "#", "1"],
   ["0dd1a7", "Help", "#", "1"],
-  ["2196F3", "WA5CAF", "https://wa5caf.github.io/", "1.7"],
+  ["2196F3", "WA5CAF", "README.md", "1.7"],
   
   [
     "2196F3",
